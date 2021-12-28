2e5eca465dff10d22e35cd94efa315ac85e8e2cf
massimoalbarello <albo.max@hotmail.it>
2021-12-28 16:05:12 +0100

    videocall on different tabs of the same browser

---
+++ js/receiver.js
@@ -0,0 +1,34 @@
+const video2 = document.querySelector('video#video2');
+const offerInput = document.getElementById("offer");
+const answerOutput = document.getElementById("answer");
+const offerBtn = document.getElementById("offerBtn");
+const form = document.getElementById("form");
+
+let remoteStream;
+
+let pc2 = new RTCPeerConnection();
+
+pc2.onicecandidate = e => {
+  console.log("New ICE candidate, reprinting SDP: " + JSON.stringify(pc2.localDescription));
+  answerOutput.innerText = JSON.stringify(pc2.localDescription);
+  form.style.display = "none";
+}
+
+pc2.onaddstream = function(e) {
+  remoteStream = e.stream;
+  video2.srcObject = remoteStream;
+  console.log(remoteStream)
+  console.log('Received remote stream');
+  
+};
+
+offerBtn.onclick = async () => {
+  const offer = offerInput.value;
+  if (offer) {
+    console.log("Setting offer: " + offer);
+    await pc2.setRemoteDescription(JSON.parse(offer));
+    console.log("Offer set!");
+    const answer = await pc2.createAnswer();
+    pc2.setLocalDescription(answer);
+  }
+}
---
+++ js/sender.js
@@ -0,0 +1,51 @@
+const video1 = document.getElementById("video1");
+const answerInput = document.getElementById("answer");
+const offerOutput = document.getElementById("offer");
+const answerBtn = document.getElementById("answerBtn");
+const form = document.getElementById("form");
+
+let localStream;
+
+function gotStream(stream) {
+  console.log('Received local stream');
+  video1.srcObject = stream;
+  localStream = stream;
+  console.log('Starting call');
+  videocall(localStream);
+}
+
+function videocall(stream) {
+  const pc1 = new RTCPeerConnection();
+  pc1.addStream(stream);
+
+  pc1.onicecandidate = e => {
+    console.log("New ICE candidate, reprinting SDP: " + JSON.stringify(pc1.localDescription));
+    offerOutput.innerText = JSON.stringify(pc1.localDescription);
+  }
+  pc1.createOffer().then(o => pc1.setLocalDescription(o)).then(a => {
+    console.log("Set successfully!");
+  });
+
+  answerBtn.onclick = () => {
+    const answer = answerInput.value;
+    if (answer) {
+      console.log("Setting answer: " + answer);
+      pc1.setRemoteDescription(JSON.parse(answer));
+      answerInput.value = "";
+      offerOutput.innerText = "";
+      form.style.display = "none";
+    }
+  }
+
+}
+
+console.log('Requesting local stream');
+const options = {audio: false, video: true};
+navigator.mediaDevices
+    .getUserMedia(options)
+    .then(gotStream)
+    .catch(function(e) {
+      alert('getUserMedia() failed');
+      console.log('getUserMedia() error: ', e);
+    });
+
