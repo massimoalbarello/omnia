73c5f8a327379b410e74baa6ee9c188db2184ecf
massimoalbarello <albo.max@hotmail.it>
2021-12-28 11:40:05 +0100

    all in one file

--- js/main.js
+++ js/main.js
@@ -12,6 +12,47 @@ function gotStream(stream) {
   new videocall(localStream, gotremoteStream);
 }
 
+function errorHandler(context) {
+  return function(error) {
+    trace('Failure in ' + context + ': ' + error.toString);
+  };
+}
+
+function noAction() {
+}
+
+function videocall(stream, handler) {
+  let pc1 = new RTCPeerConnection();
+  let pc2 = new RTCPeerConnection();
+
+  pc1.addStream(stream);
+  pc1.onicecandidate = function(event) {
+    if (event.candidate) {
+      pc2.addIceCandidate(new RTCIceCandidate(event.candidate),
+                          noAction, errorHandler('AddIceCandidate'));
+    }
+  };
+  pc2.onicecandidate = function(event) {
+    if (event.candidate) {
+      pc1.addIceCandidate(new RTCIceCandidate(event.candidate),
+                          noAction, errorHandler('AddIceCandidate'));
+    }
+  };
+  pc2.onaddstream = function(e) {
+    handler(e.stream);
+  };
+  pc1.createOffer(function(desc) {
+    pc1.setLocalDescription(desc);
+    pc2.setRemoteDescription(desc);
+    pc2.createAnswer(function(desc2) {
+      pc2.setLocalDescription(desc2);
+      pc1.setRemoteDescription(desc2);
+    }, errorHandler('pc2.createAnswer'));
+  }, errorHandler('pc1.createOffer'));
+}
+
+
+
 function gotremoteStream(stream) {
   remoteStream = stream;
   video2.srcObject = stream;
