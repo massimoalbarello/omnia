69c1da237d9cfdea9d6cc431e6da2e82fe8b3a4a
massimoalbarello <albo.max@hotmail.it>
2022-01-09 12:09:13 +0100

    browse the web with mouse

--- omniPointer/webSocketServer.js
+++ omniPointer/webSocketServer.js
@@ -2,6 +2,7 @@ const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: 6969 });
 
+
 const nearbyThings = {
     "10.4.23.47": {
         device: "pointer",
@@ -17,29 +18,36 @@ const clients = new Map();
 var currentOutputDevice;
 
 // to be removed once devices have their own ip
-var tmp = ["10.4.23.83", "10.4.23.83", "10.4.23.47"];
+var tmp = ["10.4.23.47"];
 ///////////////////////////////////////////////
 
+// !!!!!!!!!!!!!!!! start "getMouseCoordinate" first and then "moveMouseExtension" on browser !!!!!!!!!!!!!!!!
 wss.on('connection', (ws, req) => {
+
+    // to be added once devices have their own ip
     // const clientIp = req.socket.remoteAddress;
+    /////////////////////////////////////////////
+
     // to be removed once devices have their own ip
     const clientIp = tmp[tmp.length - 1];
     tmp.pop();
+    tmp.push("10.4.23.83");
     ///////////////////////////////////////////////
+
     if (nearbyThings[clientIp].device === "pointer") {
-        console.log("Connected to pointer");
         clients.set(ws, nearbyThings[clientIp]);
     }
     else if (nearbyThings[clientIp].device === "display") {
-        console.log("Connected to display");
         clients.set(ws, nearbyThings[clientIp]);
         currentOutputDevice = {
             ws: ws,
         }
     }
+    console.log("Connected to: ", nearbyThings[clientIp].device);
+    
     ws.on('message', (messageAsString) => {
         const message = JSON.parse(messageAsString);
-        console.log("Received message: ", message);
+        // console.log("Received message: ", message);
         if (message.device === "pointer") {
             if (currentOutputDevice) {
                 const metadata = clients.get(ws);
@@ -50,7 +58,7 @@ wss.on('connection', (ws, req) => {
                 currentOutputDevice.ws.send(JSON.stringify(data));
             }
             else {
-                console.log("No output device found.")
+                // console.log("No output device found.")
             }
         }
     });
