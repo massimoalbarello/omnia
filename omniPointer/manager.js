805d59e11cfba3e23eada9c69e6a901fe053e814
massimoalbarello <albo.max@hotmail.it>
2022-01-24 18:48:19 +0100

    start app from control panel

--- omniPointer/manager.js
+++ omniPointer/manager.js
@@ -1,6 +1,7 @@
-Servient = require("@node-wot/core").Servient;
-HttpClientFactory = require("@node-wot/binding-http").HttpClientFactory;
-Helpers = require("@node-wot/core").Helpers;
+const Servient = require("@node-wot/core").Servient;
+const HttpClientFactory = require("@node-wot/binding-http").HttpClientFactory;
+const Helpers = require("@node-wot/core").Helpers;
+const WebSocket = require('ws');
 
 // create Servient and add HTTP binding
 let servient = new Servient();
@@ -8,11 +9,31 @@ servient.addClientFactory(new HttpClientFactory(null));
 let wotHelper = new Helpers(servient);
 
 // list of local urls received from router
-const localURLs = ["http://localhost:8080/display", "http://localhost:8081/pointer", "http://localhost:8082/keyboard"];
+var localURLs = ["http://localhost:8080/display", "http://localhost:8081/pointer", "http://localhost:8082/keyboard"];
 
 var thingDescriptors = {};
 var servients = {};
 var things = {};
+var appIsAvailable = false;
+var availableApps = [];
+var cpws;   // control panel web socket
+var appStarted = false;
+
+// list of available apps (localhost:2000)
+const controlPanelWSS = new WebSocket.Server({ port: 2000 });
+
+controlPanelWSS.on('connection', (ws, req) => {
+    cpws = ws;
+    cpws.send(JSON.stringify(availableApps));
+
+    cpws.on('message', (message) => {
+        if (!appStarted) {
+            log(message.toString(), "$");
+            appStarted = true;
+            startwebComputer();
+        }
+    });
+});
 
 const discoverDevicesInterval = setInterval(async () => {
     for (const index in localURLs) {
@@ -24,29 +45,12 @@ const discoverDevicesInterval = setInterval(async () => {
             log("Connected to: " + url, "#");
             localURLs.splice(index, 1);
 
-            if (! localURLs.length) {   // check the requirements needed to start the app
-                clearInterval(discoverDevicesInterval);
-
-                // once the manager knows which device is which (thanks to the TDs), set up the connections between the devices           
-                const inputThings = {
-                    "http://localhost:8081/pointer": {
-                        ipAddress: "http://localhost:8081/pointer",
-                        thing: "pointer",
-                    },
-                    "http://localhost:8082/keyboard": {
-                        ipAddress: "http://localhost:8082/keyboard",
-                        thing: "keyboard",
-                    }
-                }
-
-                // get the WS where the output device listens for connectons and set the devices it will receive from
-                const outWsAddress = await things["http://localhost:8080/display"].readProperty("wsAddress");
-                things["http://localhost:8080/display"].invokeAction("inputThings", inputThings);
-
-                // set WS that input devices use to communicate their data
-                things["http://localhost:8081/pointer"].invokeAction("wsAddress", outWsAddress);
-                things["http://localhost:8082/keyboard"].invokeAction("wsAddress", outWsAddress);
-            }
+            [appIsAvailable, availableApps] = checkAppsAvailable();
+            if (appIsAvailable) {
+                localURLs = ["http://localhost:8083/stereo"];   // keep discovering for other devices, ex. stereo
+                log(availableApps, "&");
+                cpws.send(JSON.stringify(availableApps));
+            };
         }
         catch {
             // log(url + ": not available.", "!");
@@ -55,7 +59,36 @@ const discoverDevicesInterval = setInterval(async () => {
     log(localURLs, "?");
 }, 10000);
 
+function checkAppsAvailable() {
+    if (! localURLs.length) {   // check the requirements needed to start the app
+        return [true, ["web computer", "get coordinates"]];
+    }
+    return [false, []];
+}
 
+async function startwebComputer() {
+
+    log("Starting web computer!", "!");
+    // once the manager knows which device is which (thanks to the TDs), set up the connections between the devices           
+    const inputThings = {
+        "http://localhost:8081/pointer": {
+            ipAddress: "http://localhost:8081/pointer",
+            thing: "pointer",
+        },
+        "http://localhost:8082/keyboard": {
+            ipAddress: "http://localhost:8082/keyboard",
+            thing: "keyboard",
+        }
+    }
+
+    // get the WS where the output device listens for connectons and set the devices it will receive from
+    const outWsAddress = await things["http://localhost:8080/display"].readProperty("wsAddress");
+    things["http://localhost:8080/display"].invokeAction("inputThings", inputThings);
+
+    // set WS that input devices use to communicate their data
+    things["http://localhost:8081/pointer"].invokeAction("wsAddress", outWsAddress);
+    things["http://localhost:8082/keyboard"].invokeAction("wsAddress", outWsAddress);
+}
 
 function log(value, separator) {
     console.log("\n" + separator.repeat(20));
