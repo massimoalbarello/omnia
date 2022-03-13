cd1cde638a9900baf3e2ad360af603b0e4c73853
massimoalbarello <albo.max@hotmail.it>
2022-03-13 17:23:00 +0100

    scan QR codes of sender and receiver IDs

--- omniShare/public/js/scanner.js
+++ omniShare/public/js/scanner.js
@@ -1,28 +1,56 @@
 
-const OPERATOR_API_KEY = "your_operator_api_key";
+const OPERATOR_API_KEY = "JXtMocODvVVibphu7gL8t4aK9J1jIRDrqk7dnqI8qXJd4pJLWPwhTPZUHX7iF1NHTF02QuZWWHYYjBBX";
 const operator = new evrythng.Operator(OPERATOR_API_KEY);
-const thngIdSender = "VTyqPXxTCd3P3hddsKFfQhch";    // scan the QR to learn this ID
-const thngIdReceiver = "VTy6QSN4nVbRE2cg9HcFUdpp";  // scan the QR to learn this ID
+// const thngIdSender = "VTyqPXxTCd3P3hddsKFfQhch";    // scan the QR to learn this ID
+// const thngIdReceiver = "VTy6QSN4nVbRE2cg9HcFUdpp";  // scan the QR to learn this ID
 
-const urlSender = `wss://ws.evrythng.com:443/thngs/${thngIdSender}/properties/answer?access_token=${OPERATOR_API_KEY}`;
-const socketSender = new WebSocket(urlSender);
+evrythng.setup({
+    apiVersion: 1
+})
+evrythng.use(ScanThng);
 
-const urlReceiver = `wss://ws.evrythng.com:443/thngs/${thngIdReceiver}/properties/offer?access_token=${OPERATOR_API_KEY}`;
-const socketReceiver = new WebSocket(urlReceiver);
+const APPLICATION_API_KEY = 'gTibS5rdLBrH38ABAREv4WnvKvD0bqxGTdJ1ikybjRCTLy6uVtmUMsGT1zLi1V4wylWBfV8PHvYK9JHa';
+const app = new evrythng.Application(APPLICATION_API_KEY);
 
-socketReceiver.addEventListener('open', () => {
-    operator.thng(thngIdSender).read().then((thng) => {
-        const offer = thng.customFields.sdp;
-        console.log(offer);
-        const update = [{ value: offer }];
-        socketReceiver.send(JSON.stringify(update));
-        setTimeout(() => {  // wait for the offer to be recived by the receiver web app to then set its SDP on the receiver thng
-            operator.thng(thngIdReceiver).read().then((thng) => {
-                const answer = thng.customFields.sdp;
-                console.log(answer);
-                const update = [{ value: answer }];
-                socketSender.send(JSON.stringify(update));
-            });
-        }, 5000);
+var thngIdSender;
+
+async function scanner() {
+    const scanSender = await app.scanStream({
+        filter: { method: '2d', type: 'qr_code' },
+        containerId: 'stream_container',
     });
-});
+    thngIdSender = scanSender[0].meta.value
+    console.log("Sender ID: " + thngIdSender);
+
+    const scanReceiver = await app.scanStream({
+        filter: { method: '2d', type: 'qr_code' },
+        containerId: 'stream_container',
+    });
+    thngIdReceiver = scanReceiver[0].meta.value
+    console.log("Receiver ID: " + thngIdReceiver);
+        
+    const urlSender = `wss://ws.evrythng.com:443/thngs/${thngIdSender}/properties/answer?access_token=${OPERATOR_API_KEY}`;
+    const socketSender = new WebSocket(urlSender);
+
+    const urlReceiver = `wss://ws.evrythng.com:443/thngs/${thngIdReceiver}/properties/offer?access_token=${OPERATOR_API_KEY}`;
+    const socketReceiver = new WebSocket(urlReceiver);
+
+    socketReceiver.addEventListener('open', () => {
+        operator.thng(thngIdSender).read().then((thng) => {
+            const offer = thng.customFields.sdp;
+            console.log(offer);
+            const update = [{ value: offer }];
+            socketReceiver.send(JSON.stringify(update));
+            setTimeout(() => {  // wait for the offer to be recived by the receiver web app to then set its SDP on the receiver thng
+                operator.thng(thngIdReceiver).read().then((thng) => {
+                    const answer = thng.customFields.sdp;
+                    console.log(answer);
+                    const update = [{ value: answer }];
+                    socketSender.send(JSON.stringify(update));
+                });
+            }, 5000);
+        });
+    });
+}
+
+scanner();
